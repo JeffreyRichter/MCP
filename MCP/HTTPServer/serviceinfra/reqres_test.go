55e6f732aeaa1237c39f118093424737a40322ad
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2025-08-16 12:46:18 -0700

    etag tests

--- MCP/HTTPServer/serviceinfra/reqres_test.go
+++ MCP/HTTPServer/serviceinfra/reqres_test.go
@@ -3,6 +3,7 @@ package serviceinfra
 import (
 	"context"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -76,6 +77,486 @@ func TestRequestHeaderVerifyStructFields(t *testing.T) {
 			err := httpjson.VerifyStructFields(tt.rh)
 			if err != nil {
 				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestValidatePreconditions(t *testing.T) {
+	baseTime := time.Date(2024, 1, 1, 12, 0, 0, 0, time.UTC)
+
+	tests := []struct {
+		name          string
+		method        string
+		headers       map[string]string
+		preconditions PreconditionValues
+		expectedCode  int
+	}{
+		// Error cases: resource doesn't support headers
+		{
+			name:   "resource doesn't support if-match",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         nil,
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:   "resource doesn't support if-none-match",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         nil,
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:   "resource doesn't support if-modified-since",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Modified-Since": baseTime.Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: nil,
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:   "resource doesn't support if-unmodified-since",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Unmodified-Since": baseTime.Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: nil,
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+
+		// If-Match tests
+		{
+			name:   "if-match matches precondition",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-match doesn't match precondition",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("456")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusPreconditionFailed,
+		},
+		{
+			name:   "if-match weak etag fails strong comparison",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match": `W/"123"`,
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusPreconditionFailed,
+		},
+		{
+			name:   "if-match with resource weak etag fails strong comparison",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag(`W/"123"`)),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusPreconditionFailed,
+		},
+
+		// If-Match + If-None-Match tests
+		{
+			name:   "if-match and if-none-match match",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match":      "123",
+				"If-None-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified, // If-None-Match takes precedence when both match
+		},
+		{
+			name:   "if-match matches_if-none-match doesn't match_safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match":      "123",
+				"If-None-Match": "456",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-match matches_if-none-match doesn't match_unsafe method",
+			method: http.MethodPost,
+			headers: map[string]string{
+				"If-Match":      "123",
+				"If-None-Match": "456",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+
+		// If-Match + If-Modified-Since combination tests
+		{
+			name:   "if-match matches_if-modified-since_newer resource",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match":          "123",
+				"If-Modified-Since": baseTime.Add(-time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-match matches_if-modified-since_older resource safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Match":          "123",
+				"If-Modified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+		{
+			name:   "if-match matches_if-modified-since_older resource_unsafe method",
+			method: http.MethodPost,
+			headers: map[string]string{
+				"If-Match":          "123",
+				"If-Modified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+
+		// If-Unmodified-Since tests
+		{
+			name:   "if-unmodified-since_ older resource",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Unmodified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-unmodified-since_ newer resource",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Unmodified-Since": baseTime.Add(-time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusPreconditionFailed,
+		},
+
+		// If-Unmodified-Since + If-None-Match combination tests
+		{
+			name:   "if-unmodified-since passes_if-none-match matches_safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match":       "123",
+				"If-Unmodified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+		{
+			name:   "if-unmodified-since passes_if-none-match matches_unsafe method",
+			method: http.MethodPost,
+			headers: map[string]string{
+				"If-None-Match":       "123",
+				"If-Unmodified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusPreconditionFailed,
+		},
+		{
+			name:   "if-unmodified-since passes_if-none-match doesn't match",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match":       "456",
+				"If-Unmodified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+
+		// If-Unmodified-Since + If-Modified-Since combination tests
+		{
+			name:   "if-unmodified-since passes_if-modified-since_newer resource",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Unmodified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+				"If-Modified-Since":   baseTime.Add(-time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-unmodified-since passes_if-modified-since_older resource_safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Unmodified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+				"If-Modified-Since":   baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+
+		// Standalone If-None-Match tests (no If-Match, no If-Unmodified-Since)
+		{
+			name:   "if-none-match matches_safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+		{
+			name:   "if-none-match matches_unsafe method",
+			method: http.MethodPost,
+			headers: map[string]string{
+				"If-None-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusPreconditionFailed,
+		},
+		{
+			name:   "if-none-match doesn't match",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match": "456",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-none-match any_safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match": "*",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+		{
+			name:   "if-none-match any_ unsafe method",
+			method: http.MethodPost,
+			headers: map[string]string{
+				"If-None-Match": "*",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusPreconditionFailed,
+		},
+		{
+			name:   "if-none-match weak etag doesn't match_safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match": `W/"456"`,
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-none-match strong vs weak etag doesn't match_unsafe method",
+			method: http.MethodPost,
+			headers: map[string]string{
+				"If-None-Match": "456",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag(`W/"123"`)),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-none-match weak etag weak comparison matches",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-None-Match": `W/"123"`,
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag(`W/"123"`)),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+
+		// Standalone If-Modified-Since tests (no other headers)
+		{
+			name:   "if-modified-since_newer resource",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Modified-Since": baseTime.Add(-time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+		{
+			name:   "if-modified-since_older resource_safe method",
+			method: http.MethodGet,
+			headers: map[string]string{
+				"If-Modified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+		{
+			name:   "if-modified-since_older resource_unsafe method",
+			method: http.MethodPost,
+			headers: map[string]string{
+				"If-Modified-Since": baseTime.Add(time.Hour).Format(http.TimeFormat),
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+
+		// No conditional headers - success
+		{
+			name:    "no conditional headers",
+			method:  http.MethodGet,
+			headers: map[string]string{},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+		},
+
+		// Test HEAD and OPTIONS as safe methods
+		{
+			name:   "head method with if-none-match match",
+			method: http.MethodHead,
+			headers: map[string]string{
+				"If-None-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+		{
+			name:   "options method with if-none-match match",
+			method: http.MethodOptions,
+			headers: map[string]string{
+				"If-None-Match": "123",
+			},
+			preconditions: PreconditionValues{
+				ETag:         Ptr(ETag("123")),
+				LastModified: Ptr(baseTime),
+			},
+			expectedCode: http.StatusNotModified,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(tt.method, "http://localhost", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			for k, v := range tt.headers {
+				req.Header.Set(k, v)
+			}
+			rw := httptest.NewRecorder()
+			rr := NewReqRes(nil, req, rw)
+			if err = rr.ValidatePreconditions(&tt.preconditions); err != nil {
+				// this error is from writing the HTTP response
+				t.Fatal(err)
+			}
+			// ValidatePreconditions is responsible for the status code only in error
+			// cases and when preconditions aren't met as stipulated in RFC 7232
+			if tt.expectedCode == 0 {
+				// this test case expects the preconditions to hold. The "correct" code is an
+				// implementation detail we don't want to depend on here, but it certainly
+				// shouldn't indicate a precondition failure
+				switch rw.Code {
+				case http.StatusNotModified, http.StatusPreconditionFailed:
+					t.Fatalf("expected preconditions to hold but ValidatePreconditions set status code %q", http.StatusText(rw.Code))
+				}
+			} else if rw.Code != tt.expectedCode {
+				t.Fatalf("expected %q, got %q", http.StatusText(tt.expectedCode), http.StatusText(rw.Code))
 			}
 		})
 	}
